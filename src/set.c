39dfb30137b8912c84228f510d033eb3af59adfa
ramses2099 <ramses2099@gmail.com>
2026-04-07 16:04:10 -0400

    latest changes

--- src/set.c
+++ src/set.c
@@ -51,3 +51,57 @@ Set *set_union(const Set *set_a, const Set *set_b) {
 
   return new_set;
 }
+Set *set_interset(const Set *set_a, const Set *set_b) {
+  Set *new_set = init();
+  for (int i = 0; i < set_a->length; i++)
+    for (int j = 0; j < set_b->length; j++)
+      if (set_a->members[i] == set_b->members[j])
+        set_insert(new_set, set_a->members[i]);
+
+  return new_set;
+}
+
+Set *set_difference(const Set *set_a, const Set *set_b) {
+  Set *new_set = init();
+  for (int i = 0; i < set_a->length; i++) {
+    bool inB = false;
+
+    for (int j = 0; j < set_b->length; j++)
+      if (set_a->members[i] == set_b->members[j])
+        inB = true;
+
+    if (!inB) {
+      set_insert(new_set, set_a->members[i]);
+    }
+  }
+  return new_set;
+}
+
+bool is_subset(const Set *set_a, const Set *set_b) {
+  for (int i = 0; i < set_a->length; i++) {
+    bool inB = false;
+
+    for (int j = 0; j < set_b->length; j++)
+      if (set_a->members[i] == set_b->members[j])
+        inB = true;
+
+    if (!inB)
+      return false;
+  }
+  return true;
+}
+
+bool set_equality(const Set *set_a, const Set *set_b) {
+  if (set_a->length != set_b->length)
+    return false;
+  return is_subset(set_a, set_b);
+}
+
+bool is_member(Set *set, int value) {
+  for (int i = 0; i < set->length; i++) {
+    if (set->members[i] == value) {
+      return true;
+    }
+  }
+  return false;
+}
--- src/set.h
+++ src/set.h
@@ -12,5 +12,10 @@ void set_insert(Set *set, int member);
 void set_print(const Set *set);
 // Operation
 Set *set_union(const Set *set_a, const Set *set_b);
+Set *set_interset(const Set *set_a, const Set *set_b);
+Set *set_difference(const Set *set_a, const Set *set_b);
+bool is_subset(const Set *set_a, const Set *set_b);
+bool set_equality(const Set *set_a, const Set *set_b);
+bool is_member(Set *set, int value);
 
 #endif // !SET_H
